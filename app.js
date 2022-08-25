bc77efb6e27a0d363352d42b0aa2b4587afa4f78
Nox1do <noogux@gmail.com>
2022-08-24 22:52:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let adjs = ['great', 'big'];
 let nouns = ['jogger', 'racoon'];
 let dots = [".com", ".lol", ".gg", ".app"]
 
-//for loop anidado para mezclar los items de los arrays
+//for loop nested para mezclar los items de los arrays
 for (let i = 0; i < adjs.length; i++) { // bucle padre
 
     for (let j = 0; j < pronouns.length; j++) {
